d091bedbc39bb158627c6b9d7316e3808be8067e
yaqootturman <yaqoot.turman@gmail.com>
2020-08-09 20:28:01 +0300

    change hight of main div

--- client/src/Components/Greeting/Greeting.js
+++ client/src/Components/Greeting/Greeting.js
@@ -11,7 +11,7 @@ const Greeting = () => {
           <Col xs={6} >
             <h1 className="greeting__statement">Save <span className="greeting__statement__word"> Money.</span><br />Live Better.</h1>
             <p>I still have my feet on the ground, i just wear better shoes tag clipped to my shirt with my name printed on it.</p>
-            <Button variant="link">SHOP NOW</Button>
+            <Button className="greeting__btn" variant="link">SHOP NOW</Button>
           </Col>
           <Col xs={6} className="greeting__chair-div" >
             <Image className="greeting__chair-div__chair" src={chair} />
