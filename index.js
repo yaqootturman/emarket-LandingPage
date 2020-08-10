28fee00965376b818746f2553fb04e840b36c31b
yaqootturman <yaqoot.turman@gmail.com>
2020-08-10 09:37:33 +0300

    add index of server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const publicPath = path.join(__dirname, '..', 'client', 'public');
 app.use(express.static(publicPath));
