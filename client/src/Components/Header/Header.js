4b8a1dc6f9cbfe9038550606714205d12290bc46
yaqootturman <yaqoot.turman@gmail.com>
2020-08-10 10:50:27 +0300

    smooth scrolling

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -15,10 +15,17 @@ const Header = () => {
 
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav className="mr-auto" defaultActiveKey="greeting" className="justify-content-end">
-          <Nav.Item as="li"><Nav.Link eventKey="greeting"><Link to="greeting">Home</Link></Nav.Link></Nav.Item>
-          <Nav.Item as="li"><Nav.Link eventKey="products"><Link to="products">Products</Link></Nav.Link> </Nav.Item>
-          <Nav.Item as="li"> <Nav.Link eventKey="collection"><Link to="collection">Collections</Link></Nav.Link></Nav.Item>
-          <Nav.Item as="li"> <Nav.Link eventKey="email-section"><Link to="email-section">Contact</Link></Nav.Link> </Nav.Item>
+          <Nav.Item as="li"><Nav.Link eventKey="greeting"><Link to="greeting" smooth={true}
+          >Home</Link></Nav.Link></Nav.Item>
+          <Nav.Item as="li"><Nav.Link eventKey="products"><Link to="products" offset={-70}
+            smooth={true}
+          >Products</Link></Nav.Link> </Nav.Item>
+          <Nav.Item as="li"> <Nav.Link eventKey="collection"><Link to="collection" offset={-70}
+            smooth={true}
+          >Collections</Link></Nav.Link></Nav.Item>
+          <Nav.Item as="li"> <Nav.Link eventKey="email-section"><Link to="email-section" offset={-70}
+            smooth={true}
+          >Contact</Link></Nav.Link> </Nav.Item>
         </Nav>
       </Navbar.Collapse>
 
